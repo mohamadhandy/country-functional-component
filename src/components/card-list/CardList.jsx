b693653bc9f5e4c3fc7003ffacea538961979fb6
mohamadhandy <108653540+mohamadhandy@users.noreply.github.com>
2022-11-16 17:54:56 +0000

    destructuring props in argument and use implicit return

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import Card from '../card/Card'
 import "./cardlist.css"
 
-const CardList = ({ countries }) => {
-  return (
-    <div className="card-list">
-      {countries.map(country => {
-        return <Card key={country.name} countries={country} />
-      })}
-    </div>
-  )
-}
+const CardList = ({ countries }) => (
+  <div className="card-list">
+    {countries.map(country => {
+      return <Card key={country.name} country={country} />
+    })}
+  </div>
+)
+
 
 export default CardList
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,28 +1,26 @@
 import React from 'react'
 import "./card.css"
 
-const Card = (props) => {
-  const { name, capital, flags, demonym, population } = props.countries
-  return (
-    <div className="card-container">
-      <h1>{name}</h1>
-      <img
-        alt="Country"
-        src={flags.png}
-        width={'300px'}
-        height={'200px'}
-      />
-      <span>
-        Capital of {name} is {capital}
-      </span>
-      <span>
-        Demonym of {name} is {demonym}
-      </span>
-      <span>
-        {name}'s population {population.toLocaleString()}
-      </span>
-    </div>
-  )
-}
+const Card = ({ country: { name, capital, flags, demonym, population } }) => (
+  <div className="card-container">
+    <h1>{name}</h1>
+    <img
+      alt="Country"
+      src={flags.png}
+      width={'300px'}
+      height={'200px'}
+    />
+    <span>
+      Capital of {name} is {capital}
+    </span>
+    <span>
+      Demonym of {name} is {demonym}
+    </span>
+    <span>
+      {name}'s population {population.toLocaleString()}
+    </span>
+  </div>
+)
+
 
 export default Card
